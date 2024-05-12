0c6e28d956b8f23e34d5c0d294ff7f20145338ea
christianadvincula <christianadvincula@gmail.com>
2024-05-12 12:21:41 +0800

    corrected Controller to Service Not Repository

--- src/main/java/guru/springframework/spring6webapp/controllers/AuthorController.java
+++ src/main/java/guru/springframework/spring6webapp/controllers/AuthorController.java
@@ -1,20 +1,22 @@
 package guru.springframework.spring6webapp.controllers;
 
 import guru.springframework.spring6webapp.repositories.AuthorRepository;
+import guru.springframework.spring6webapp.services.AuthorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AuthorController {
-    private final AuthorRepository authorRepository;
-    public AuthorController(AuthorRepository authorRepository) {
-        this.authorRepository = authorRepository;
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
     }
 
     @RequestMapping("/authors")
     public String getAuthors(Model model) {
-        model.addAttribute("authors", authorRepository.findAll());
+        model.addAttribute("authors", authorService.findAll());
         return "authors";
     }
 }
